663893f4409ddf18bc123ef6386a902542de8478
Suraj Kumar <suraj.kr1450@gmail.com>
2022-06-27 11:21:56 +0530

    todolist

---
+++ script.js
@@ -0,0 +1,64 @@
+// let inputTag = document.querySelector("input");
+// let ulTag = document.querySelector("ul");
+
+// inputTag.addEventListener('keydown',function(e){
+//     // console.log(e);
+//     let key = e.key;
+//     if(key == 'Enter'){
+//         let value = inputTag.value;
+//         // console.log(value);
+//         if(value.length == 0){
+//             return;
+//         }
+//         inputTag.value = ""; //ui input update 
+//         let liTag = document.createElement("li");
+//         liTag.innerHTML = `<div>${value}</div>
+//                             <div class="delete"><i class="fa fa-trash"></i></div>`
+
+//         //other way
+//         // liTag.innerHTML = "<div>"+value+"</div><div><i class='fa fa-trash'></i></div>"
+//         handleRemoval(liTag);
+//         ulTag.appendChild(liTag);
+//     }
+// })
+
+// function handleRemoval(liTag){
+//     let deleteDiv = liTag.querySelector(".delete"); 
+//     deleteDiv.addEventListener("click",function(){
+//         liTag.remove();
+//     })
+// }
+
+
+
+let inputTag = document.querySelector("input");
+let ulTag = document.querySelector("ul");
+
+inputTag.addEventListener('keydown', function(e){
+   // console.log(e)
+   let key = e.key;
+   if(key == 'Enter'){
+    let value = inputTag.value;
+
+    if(value.length == 0){
+      return;
+    }
+    //console.log(value);
+    inputTag.value = "";  // isse ui ka input update hoga
+    let liTag = document.createElement("li");     // element ko create kiya
+    liTag.innerHTML= `<div>${value}</div>
+    <div class = "delete"><i class = "fa fa-trash"></i></div>`  // string interpolation-> ` string${variable}string `                     // uske andar value fill kiya
+    handleRemoval(liTag);
+   ulTag.appendChild(liTag);
+   }
+
+})
+
+function handleRemoval(liTag){
+   let deleteDiv = liTag.querySelector(".delete");
+   deleteDiv.addEventListener("click", function(){
+      liTag.remove();
+   })
+}
+
+
